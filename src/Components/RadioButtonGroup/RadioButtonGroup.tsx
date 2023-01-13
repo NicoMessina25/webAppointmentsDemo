db97e233788ddb7fe1325f2e9eeef3f2c29791da
NicoMessina25 <nicolasmessina25@gmail.com>
2023-01-13 13:00:19 -0300

    actualización del radioButtonGroup: permite agregar captions a las opciones

--- src/Components/RadioButtonGroup/RadioButtonGroup.tsx
+++ src/Components/RadioButtonGroup/RadioButtonGroup.tsx
@@ -8,13 +8,17 @@ export default function RadioButtonGroup({options, value, setValue, labelId, cla
 
     return(
         <div className={`flexible--column radioBtnGContainer ${className}`}>
-            <p>{intl.formatMessage({id: labelId})}:</p>
+            {labelId && <p className="radioButtonLabel">{intl.formatMessage({id: labelId})}:</p>}
             <div className={`flexible--${orientation}Wrap optionsContainer`}>
                 {options.map((op:any, ind:number)=>{
                     return <div className="field-radiobutton flexible--row" key={`${op.label}-${ind}`}>
-                            <RadioButton  inputId={`${labelId}${ind}`} name={labelId} value={op.label} onChange={(e) => setValue(e.value)} checked={value === op.label} />
-                            <label htmlFor={`${labelId}${ind}`}>{op.label}</label>
-                        </div>
+                                
+                                {op.captions && <label htmlFor={`${labelId}${ind}`} className="radioButtonLabel">{op.captions}:</label>}
+
+                                <RadioButton  inputId={`${labelId}${ind}`} name={labelId} value={op.label} onChange={(e) => setValue(e.value)} checked={value === op.label} />
+
+                                <label htmlFor={`${labelId}${ind}`}>{op.label}</label>
+                            </div>
                 })}
             </div>
             
