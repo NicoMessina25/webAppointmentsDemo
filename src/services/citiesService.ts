1d73032b365e7366d80e66c560a415793f99cbc4
TadeoRimoli <taderimoli@gmail.com>
2023-01-23 12:58:36 -0300

    getcities

--- src/services/citiesService.ts
+++ src/services/citiesService.ts
@@ -1,6 +1,15 @@
 import axios from "axios";
 
 
-export function getAllCities(){
-    return axios.get(process.env.REACT_APP_MEDERE_ADDRESS +  '/rest/frontend/providers/getcities').then(res => res.data)
+export function getAllCities(inputText:any,languageId:any){
+    
+    console.log(inputText," ",languageId)
+    return axios.post(process.env.REACT_APP_MEDERE_ADDRESS +  '/rest/webappointments/getCities'
+    ,{
+        params:{
+            languageCode:languageId,
+            inputText:inputText
+        }
+    }
+    ).then(res => res.data)
 }
